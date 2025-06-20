578e30ec2bec78521c300f448081fbf3cbfe4e91
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-06-20 19:14:01 -0300

    test: installation and configuration of coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
+       reportsDirectory: './coverage',
     },
     include: ['src/**/*.{test,spec}.{ts,js}']
   },
