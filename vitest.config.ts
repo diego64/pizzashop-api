623ca34769288ec7a109879be984f2fad709795d
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-06-20 15:19:34 -0300

    feat: configuration for testing

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: ['src/test/setup.ts'],
+    testTimeout: 50_000,
+    hookTimeout: 30_000,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+    include: ['src/**/*.{test,spec}.{ts,js}']
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+})
