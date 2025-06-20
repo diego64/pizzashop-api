03716fd6154b01dcf3a2644db2bbf271f1193701
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-06-20 16:12:02 -0300

    test: mook file

---
+++ __mocks__/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+  JWT_SECRET: 'test-secret',
+  DATABASE_URL:'postgresql://postgresql://test:test@localhost:5432/pizzashop_test',
+  API_BASE_URL:"http://localhost",
+  AUTH_REDIRECT_URL:"http://localhost",
+  RESEND_API_KEY:"test-key"
+}
