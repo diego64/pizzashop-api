86f52af9cbbc3bb4f287b7c69caaf0a19e48d305
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-06-28 19:39:37 -0300

    refactor: file correction for running tests

--- src/utils/check-database-connection.ts
+++ src/utils/check-database-connection.ts
@@ -1,9 +1,10 @@
 import { client } from '@/db/connection'
 
-export async function checkDatabaseConnection() {
+export async function checkDatabaseConnection(): Promise<boolean> {
   try {
     await client`SELECT 1`
     console.log('====== Database connection established successfully! ======')
+    return true
   } catch (error) {
     console.error('Error connecting to the database:', error)
     process.exit(1)
