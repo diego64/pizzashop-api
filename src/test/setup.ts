3ec2d2ce78c1c8d4f34c84cc37e7ec611061e7c1
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-06-28 12:40:25 -0300

    chore: adjustment in the file

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,5 @@
 import { mockEnv } from '../../__mocks__/env'
+import { defineConfig } from 'vitest/config'
 import dotenv from 'dotenv'
 import path from 'path'
 
@@ -9,3 +10,10 @@ dotenv.config({
 for (const [key, value] of Object.entries(mockEnv)) {
   process.env[key] = value
 }
+
+export default defineConfig({
+  test: {
+    globals: true,
+    setupFiles: ['test/setup.ts'],
+  },
+})
